fa05b0684c48365edc9666ada72af7776ecdd27e
k-gupta2518 <you@example.com>
2024-08-07 22:30:46 +0530

    responsive commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Todo from './components/Todo'
+import React from 'react';
+import Todo from './components/Todo';
 
 const App = () => {
   return (
-    <div className='bg-sky-900	grid py-4 min-h-screen'>
-      <Todo/>  
+    <div className='bg-sky-900 py-4 min-h-screen flex items-center justify-center'>
+      <div className="w-full max-w-md mx-auto p-4">
+        <Todo />  
       </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
